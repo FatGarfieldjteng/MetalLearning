bb073a3e0e9a6a200ca544d73da087028d56115c
FatGarfieldjteng <fatgarfieldjteng@hotmail.com>
2023-12-15 16:07:56 +0800

    Draw a triangle.

--- MetalLearning/Render.swift
+++ MetalLearning/Render.swift
@@ -5,4 +5,115 @@
 //  Created by Tengjun on 12/15/23.
 //
 
-import Foundation
+import MetalKit
+
+class Renderer: NSObject{
+    
+    static var _device: MTLDevice!
+    static var _commandQueue: MTLCommandQueue!
+    static var _library: MTLLibrary!
+    var _vertexBuffer: MTLBuffer!
+    var _pipelineState: MTLRenderPipelineState!
+    
+    
+    var _mesh: MTKMesh!
+    
+    init(metalView: MTKView){
+        guard
+            let device = MTLCreateSystemDefaultDevice(),
+            let commandQueue = device.makeCommandQueue() else{
+            fatalError("GPU not available")
+        }
+        Renderer._device = device;
+        Renderer._commandQueue = commandQueue
+        metalView.device = device
+        
+        // create mesh
+        let allocator = MTKMeshBufferAllocator(device: device)
+        let size: Float = 0.8
+        let mdlMesh = MDLMesh(boxWithExtent: [size, size, size], segments: [1, 1, 1], inwardNormals: false, geometryType: .triangles, allocator: allocator)
+        
+        do{
+            _mesh = try MTKMesh(mesh: mdlMesh, device: device)
+        }
+        catch let error{
+            print(error.localizedDescription)
+        }
+        
+        // create VB
+        _vertexBuffer = _mesh.vertexBuffers[0].buffer
+        
+        // create shaders
+        let library = device.makeDefaultLibrary()
+        Renderer._library = library
+        let vertexFunction = library?.makeFunction(name: "vertex_main")
+        let fragmentFunction = library?.makeFunction(name: "fragment_main")
+        
+        // create PSO
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
+        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlMesh.vertexDescriptor)
+        
+        do{
+            _pipelineState =
+            try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        }
+        catch let error{
+            fatalError(error.localizedDescription)
+        }
+        
+        super.init()
+        
+        metalView.clearColor = MTLClearColor(
+            red: 1.0,
+            green: 1.0,
+            blue: 0.8,
+            alpha: 1.0)
+        metalView.delegate = self
+    }
+}
+
+extension Renderer: MTKViewDelegate{
+    func mtkView(
+        _ view: MTKView,
+        drawableSizeWillChange: CGSize
+    )
+    {
+        
+    }
+    
+    func draw(in view: MTKView){
+        guard
+            let commandBuffer = Renderer._commandQueue.makeCommandBuffer(),
+            let descriptor = view.currentRenderPassDescriptor,
+            let renderEncoder =
+                commandBuffer.makeRenderCommandEncoder(descriptor: descriptor)
+        else{
+            return
+        }
+        
+        //drawing
+        renderEncoder.setRenderPipelineState(_pipelineState)
+        renderEncoder.setVertexBuffer(_vertexBuffer, offset: 0, index: 0)
+        for submesh in _mesh.submeshes{
+            renderEncoder.drawIndexedPrimitives(
+                type: .triangle,
+                indexCount: submesh.indexCount,
+                indexType: submesh.indexType,
+                indexBuffer: submesh.indexBuffer.buffer,
+                indexBufferOffset: submesh.indexBuffer.offset)
+        }
+        
+        renderEncoder.endEncoding()
+        
+        guard let drawable = view.currentDrawable
+        else{
+            return
+        }
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+}
--- MetalLearning/shaders.metal
+++ MetalLearning/shaders.metal
@@ -8,4 +8,16 @@
 #include <metal_stdlib>
 using namespace metal;
 
+struct VertexIn{
+    float4 posiiton [[attribute(0)]];
+};
 
+vertex float4 vertex_main(const VertexIn vertexIn [[stage_in]])
+{
+    return vertexIn.posiiton;
+}
+
+fragment float4 fragment_main()
+{
+    return float4(1, 0, 0, 1);
+}
