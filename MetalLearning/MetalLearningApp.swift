eb4be6727e7e2aac0917fd0681ae76de11b13607
FatGarfieldjteng <fatgarfieldjteng@hotmail.com>
2023-12-15 12:39:23 +0800

    Initial Commit

---
+++ MetalLearning/MetalLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLearningApp.swift
+//  MetalLearning
+//
+//  Created by Tengjun on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MetalLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
